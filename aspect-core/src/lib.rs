c41c04686facef430ce8b0d89b0a7dbff718a52d
Simon Chemouil <simon.chemouil@lambdacube.fr>
2019-02-06 18:15:41 +0100

    Some documentation

--- aspect-core/src/lib.rs
+++ aspect-core/src/lib.rs
@@ -1,3 +1,12 @@
+//! # An Aspect Toolkit for Rust
+//! 
+//! Aspect-RS is a project aiming to provide common ground for the main Aspect-Oriented use cases in Rust. By leveraging the trait system, declarative and procedural macros, Aspect-RS provides blocks that let you wrap methods with your custom logic.
+//! 
+//! The project has been extracted from the [Metered project](https://github.com/magnet/metered-rs), which uses the technique to build metrics that can work on expressions or methods, whether they're `async` or not. The technique seemed general enough to be in its own crate and see if it is of any interest to other parties.
+//! 
+//! Aspect-RS provides "pointcut" traits when entering or exiting an expression (`OnEnter` and `OnResult`), experimental `Update` and `UpdateRef` traits that can use parameter shadowing to intercept and update method parameters, and weaving constructs useful when building procedural macros. Please look at the [Metered project](https://github.com/magnet/metered-rs) to see Aspect-RS in action.
+
+
 pub mod update;
 
 pub trait Enter {
